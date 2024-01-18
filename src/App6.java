e53c6d272804932fa65bd3da067ae9ee8c0b2ac6
curtyraissa <raissacurty@gmail.com>
2024-01-18 01:50:16 +0000

    lista 6 e 7

---
+++ src/App6.java
@@ -0,0 +1,9 @@
+// Vamos criar uma aplicação Java que será um calendário de feriados! Para isso, queremos:
+
+// - Uma função que imprime a lista com todos os feriados
+// - Uma função que recebe uma data como parâmerto e retorna uma mensagem.
+//     - Se o dia não for feriado, imprime: “Dia <data-inserida> não é feriado 🥲”
+//     - Se o dia for feriado, retorna: “Dia <data-inserida> é <nome-do-feriado>! 🎉”
+// - Os feriados devem ser objetos criados a partir de uma classe. Lembre-se de utilizar o conceito de encapsulamento (com `getters` e `setters` conforme a necessidade).
+// - Utilize mais parâmetros e funções caso sinta necessidade.
+
---
+++ src/App7.java
@@ -0,0 +1,4 @@
+// Transforme o exercício anterior em uma API! As rotas à seguir são correspondentes às funções descritas:
+
+// - **GET** `/holidays`
+// - **GET** `/holidays/:date` (data no formato `30-05-2024`)
